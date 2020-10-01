edc00dadb0036a18fb45d90087c4adfcb05800bb
SS Johnson <scott.johnson623@gmail.com>
2020-10-01 12:50:07 +0900

    First commit

---
+++ src/components/ForecastCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import date from "date-and-time";
+
+export default function ForecastCard(props) {
+  return (
+    <div className="card">
+      <img src={props.data.day.condition.icon} />
+      <div className="spacebetween">
+        <span> {props.data.date} </span>
+        <span>{date.transform(props.data.date, "YYYY-MM-DD", "ddd")}</span>
+      </div>
+      {props.data.day.condition.text} <br />
+      <div className="spacebetween">
+        <span>Max: {Math.round(props.data.day.maxtemp_c)} °C </span>
+        <span>
+          Min:
+          {Math.round(props.data.day.mintemp_c)} °C
+        </span>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Form.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { MDBInput } from "mdbreact";
+
+export default function Form() {
+  return (
+    <>
+      <input placeholder="Enter Zipcode Here" />
+      <button>Submit</button>
+    </>
+  );
+}
